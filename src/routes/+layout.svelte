d229900469b6c19a2caff1e088f371dc7ec33c16
Nicholas Perell <nicholasperell@gmail.com>
2025-05-18 17:46:58 -0400

    Revert "began on wireframe"
    This reverts commit 6cd5b30f92364fefa24b3a49af6788fff65c9fa3.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,5 @@
 	import '../app.css';
 	let { children } = $props();
 </script>
-<div class="w-full h-full p-4 flex flex-row flex-nowrap bg-artemis-card items-center justify-center gap-12">
-	<a class="text-xl font-semibold text-white align-middle" href="./">Experience</a>
-	<a class="text-xl font-semibold text-white align-middle" href="./">Resume</a>
-	<a class="flex flex-row flex-nowrap items-center gap-5" href="./">
-		<img class="h-16" src="./imgs/goddess-icon.png" alt="Artemis Icon"/>
-		<p class="text-5xl font-bold text-white align-middle">Nicholas Perell</p>
-	</a>
-	<a class="text-xl font-semibold text-white align-middle" href="./">Blog</a>
-	<a class="text-xl font-semibold text-white align-middle" href="./">Contact</a>
-</div>
 
 {@render children()}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,18 +4,7 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {
-			colors: {
-				'artemis': {
-					'card': '#477143',
-					'primary': '#629463',
-					'secondary': '#DEFCB9',
-				},
-				'copy': {
-					DEFAULT: 'var(--np-copy-text-color)'
-				}
-			}
-		}
+		extend: {}
 	},
 
 	plugins: []
