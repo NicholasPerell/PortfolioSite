e1c6ad5e6c71878ebffd48b077a5bffc4c2156fb
Nicholas Perell <nicholasperell@gmail.com>
2025-06-23 22:23:59 -0400

    Rack for game title out of tiles

--- src/routes/(work)/addagrams/+page.svelte
+++ src/routes/(work)/addagrams/+page.svelte
@@ -1,14 +1,41 @@
 <script lang="ts">
+	import AddagramsTile from "$lib/comps/AddagramsTile.svelte";
+
     let tileInner: HTMLElement|undefined = $state();
-    let padding = $derived((tileInner?.scrollHeight ?? 0) * .125);
+    let padding = $derived((tileInner?.scrollHeight ?? 0) * .125 / 2);
     let letter: HTMLElement|undefined = $state();
     let size = $derived((letter?.scrollHeight ?? 0) * 1.25);
 </script>
 
-<div class="bg-adda-backdrop w-full">
-     <div class="bg-adda-edge aspect-square h-80 rounded-2xl">
-        <div bind:this={tileInner} style:padding={`${padding}px`} class="bg-adda-tile w-full h-[88.88%] rounded-2xl">
-            <div bind:this={letter} style:font-size={`${size}px`} style:transform={`translateY(${size / 16}px)`} class="text-adda-letter font-adda flex justify-center items-center w-full h-full">A</div>
+
+<div class="bg-adda-backdrop w-full flex items-center justify-center ">
+     <div class="flex items-center justify-center max-w-screen-lg py-40">
+        <div class="border-adda-rackline bg-adda-rack h-48 border-y-8 border-r-8 p-2 first:border-l-8 first:rounded-l-2xl last:rounded-r-2xl">
+            <AddagramsTile text="A" />
+        </div>
+        <div class="border-adda-rackline bg-adda-rack h-48 border-y-8 border-r-8 p-2 first:border-l-8 first:rounded-l-2xl last:rounded-r-2xl">
+            <AddagramsTile text="D" />
+        </div>
+        <div class="border-adda-rackline bg-adda-rack h-48 border-y-8 border-r-8 p-2 first:border-l-8 first:rounded-l-2xl last:rounded-r-2xl">
+            <AddagramsTile text="D" />
+        </div>
+        <div class="border-adda-rackline bg-adda-rack h-48 border-y-8 border-r-8 p-2 first:border-l-8 first:rounded-l-2xl last:rounded-r-2xl">
+            <AddagramsTile text="A" />
+        </div>
+        <div class="border-adda-rackline bg-adda-rack h-48 border-y-8 border-r-8 p-2 first:border-l-8 first:rounded-l-2xl last:rounded-r-2xl">
+            <AddagramsTile text="G" />
+        </div>
+        <div class="border-adda-rackline bg-adda-rack h-48 border-y-8 border-r-8 p-2 first:border-l-8 first:rounded-l-2xl last:rounded-r-2xl">
+            <AddagramsTile text="R" />
+        </div>
+        <div class="border-adda-rackline bg-adda-rack h-48 border-y-8 border-r-8 p-2 first:border-l-8 first:rounded-l-2xl last:rounded-r-2xl">
+            <AddagramsTile text="A" />
+        </div>
+        <div class="border-adda-rackline bg-adda-rack h-48 border-y-8 border-r-8 p-2 first:border-l-8 first:rounded-l-2xl last:rounded-r-2xl">
+            <AddagramsTile text="M" />
+        </div>
+        <div class="border-adda-rackline bg-adda-rack h-48 border-y-8 border-r-8 p-2 first:border-l-8 first:rounded-l-2xl last:rounded-r-2xl">
+            <AddagramsTile text="S" />
         </div>
      </div>
 </div>
