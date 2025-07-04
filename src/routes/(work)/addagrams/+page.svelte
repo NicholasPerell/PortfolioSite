d9c05971e05342c64b7635b0c94c2aa1f07a58fd
Nicholas Perell <nicholasperell@gmail.com>
2025-07-04 17:47:38 -0400

    Added Noah Quote

--- src/routes/(work)/addagrams/+page.svelte
+++ src/routes/(work)/addagrams/+page.svelte
@@ -46,11 +46,9 @@
 		<div class="flex flex-col md:grid w-full grid-cols-2 gap-3 md:gap-5 leading-7">
 			<p>
 				<span class="italic">Addagrams</span> is a tile-based word game on mobile focused on giving players the creative freedom to rearrange the letters into the words they can come up with in their own vocabulary.
-                I was contracted by 1Ton Games to get the game to a releasable state. I refactored existing architecture, and implemented new features in collaboration with Design.
-                <span class="italic">Addagrams</span> launched May 2023 on both Android and iOS, garnering 5K downloads in the 1st month.
 			</p>
 			<div class="flex w-full flex-col gap-0">
-				<p class="italic">Freelance Game Programmer</p>
+				<p class="italic">Game Programmer</p>
 				<p>Team Size: 2</p>
 				<p>Scale: Mobile Game</p>
 				<p>Development Period: March 2023–August 2023</p>
@@ -74,10 +72,41 @@
                 <img class="h-full" src="./imgs/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.svg" alt="Get It On Google Play">
             </a>
         </div>
-    </div>
-</div>
 
-<div>
-    "Chat with Nicholas Perell for an hour and you'll find he's a bright, dedicated, and empathetic team member willing to jump in head first to get the job done. After working with him ... my only regret is that I couldn't keep him for longer. You'd be wise to scoop him up."
-   ~ Noah Rosenfield, CEO of 1Ton Games
+		<div class="my-1 h-0.5 w-full bg-black dark:bg-white"></div>
+
+        <SectionHeader header="Development of Addagrams"/>
+
+        <div class="flex gap-5">
+            <div class="flex flex-col gap-5">
+                <div class="bg-adda-backdrop p-5 rounded font-adda text-xl font-semibold">
+                    <span class="text-2xl font-black">&ldquo;</span>Chat with Nicholas Perell for an hour and you'll find he's a bright, dedicated,
+                    and empathetic team member willing to jump in head first to get the job done. After working with him ... my only regret is that
+                    I couldn't keep him for longer. You'd be wise to scoop him up.<span class="text-2xl font-black">&rdquo;</span>
+                </div>
+                <div class="flex gap-3 items-center px-5">
+                    <div class="bg-black dark:bg-white h-px w-5"></div>
+                    <div class="text-xl font-bold">Noah Rosenfield, CEO of 1Ton Games</div>
+                </div>
+            </div>
+            <div class="relative h-full w-full m-5 mt-0 aspect-square">
+                <img 
+                    src="\imgs\NoahRosenfield-headshot.jpg" 
+                    alt="Noad Rosenfield with a blue baseball cap that cheekily have letter tiles velcroed to the two rows spelling out the phrase &quot;Wordle Sucks&quot;" 
+                    class="border-4 border-adda-rackline rounded-full aspect-square"
+                />
+                <img
+                    src="\imgs\noah-addagrams-profile-tile.png"
+                    alt="Noah's tile avatar that appears on the leaderboard from time to time when he ranks on the daily. It's a squared Grapefruit slice with the letters for &quot;Noah&quot; on the four quadrants."
+                    class="absolute right-0 bottom-0 w-1/3 h-1/3 object-contain"
+                />
+            </div>
+        </div>
+
+        <div>
+            I was contracted by 1Ton Games to get the game to a releasable state. I refactored existing architecture, and implemented new features
+            in collaboration with Design. <span class="italic">Addagrams</span> launched May 2023 on both Android and iOS, garnering 5K downloads
+            in the 1st month.
+        </div>
+    </div>
 </div>
