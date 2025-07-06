34ec00a0ed99b1d044714c269fc49fdfce8a84d3
Nicholas Perell <nicholasperell@gmail.com>
2025-07-06 12:40:20 -0400

    dark mode for backdrop of posts page

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -2,14 +2,21 @@
 	import BlogPostCard from '$lib/comps/BlogPostCard.svelte';
     import type { PageServerData } from './$types';
 
-    export let data: PageServerData;
-    const {posts} = data;
+    let page:  PageServerData = $props();
+    const { data } = page;
+    const { posts } = data;
+    let innerHeight = $state(0);
+    let pageBody : HTMLDivElement | undefined = $state();
+    let pageBodyMinHeight = $derived(innerHeight - (pageBody?.offsetTop ?? 0));
 </script>
 
+<svelte:window bind:innerHeight={innerHeight}/>
+
+<div bind:this={pageBody} class="w-full h-full flex grow flex-col shrink-0 bg-white dark:bg-black" style:min-height={`${pageBodyMinHeight}px`}>
 <div class="flex flex-row flex-nowrap gap-2 items-center">
-    <div class="h-0.5 w-7 bg-black"></div>
-    <p class="font-serif font-bold text-3xl text-black">Posts</p>
-    <div class="h-0.5 w-7 bg-black"></div>
+    <div class="h-0.5 w-7 bg-black dark:bg-white"></div>
+    <p class="font-serif font-bold text-3xl text-black dark:text-white">Posts</p>
+    <div class="h-0.5 w-7 bg-black dark:bg-white"></div>
 </div>
 <div class="w-full flex flex-row flex-wrap gap-6">
     {#each posts as post}
@@ -25,4 +32,5 @@
             />
         </div>
     {/each}
+</div>
 </div>
