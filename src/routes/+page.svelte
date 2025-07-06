c01898aa0e18b88361884ed35c1ab34afe4e1e1d
Nicholas Perell <nicholasperell@gmail.com>
2025-07-06 12:39:57 -0400

    dark mode coloring for home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
     let featureWorks = works.slice(0, 4);
 
 </script>
-<div class="w-full bg-gray-100 p-6 flex justify-center text-black">
+<div class="w-full bg-gray-100 dark:bg-neutral-950 p-6 flex justify-center text-black dark:text-white">
     <div class="max-w-screen-lg w-full flex gap-5">
         <div class="flex flex-col gap-2">
             <p class="text-lg font-semibold"><span class="hover:text-xl hover:font-bold duration-300">Ahoy!</span> My name is Nicholas Perell.</p>
@@ -47,12 +47,12 @@
         <img class="aspect-square w-full" src=".\imgs\nick\seattle-falls-shot.jpg" alt="Nicholas Perell" />
     </div>
 </div>
-<div class="w-full p-6 flex justify-center overflow-hidden">
+<div class="w-full p-6 flex justify-center overflow-hidden bg-white dark:bg-black">
     <div class="max-w-screen-lg w-full flex flex-col gap-5">
         <div class="flex flex-row flex-nowrap gap-2 items-center">
-            <div class="h-0.5 w-7 bg-black"></div>
-            <p class="font-serif font-bold text-3xl text-black">Posts</p>
-            <div class="h-0.5 w-7 bg-black"></div>
+            <div class="h-0.5 w-7 bg-black dark:bg-white"></div>
+            <p class="font-serif font-bold text-3xl text-black dark:text-white">Posts</p>
+            <div class="h-0.5 w-7 bg-black dark:bg-white"></div>
         </div>
         <div class="w-full flex justify-center">
         <div class="w-full flex flex-row flex-nowrap gap-6">
@@ -70,15 +70,15 @@
         </div>
         </div>
         <div class="w-full flex flex-row justify-end">
-            <a class="text-right text-lg underline w-fit hover:no-underline text-neutral-700" href="/posts">
+            <a class="text-right text-lg underline w-fit hover:no-underline text-neutral-700 dark:text-gray-300" href="/posts">
                 More →
             </a>
         </div>
     </div>
 </div>
-<div class="w-full bg-gray-100 p-6 flex justify-center">
+<div class="w-full bg-gray-100 p-6 flex justify-center dark:bg-neutral-950 text-black dark:text-white">
     <div class="max-w-screen-lg w-full flex flex-col gap-8">
-        <p class="font-bold text-3xl text-black">
+        <p class="font-bold text-3xl text-black dark:text-white">
             Experience
         </p>
         <div class="max-w-screen-lg w-full flex items-center">
@@ -89,15 +89,15 @@
             </div>
         </div>
         <div class="w-full flex flex-row justify-center">
-            <a class="text-center text-xl font-bold underline w-fit hover:no-underline text-neutral-700" href="/work">
+            <a class="text-center text-xl font-bold underline w-fit hover:no-underline text-neutral-700 dark:text-gray-300" href="/work">
                 See More →
             </a>
         </div>
     </div>
 </div>
-<div class="w-full bg-gray-200/65 p-8 flex justify-center">
+<div class="w-full bg-gray-200 dark:bg-neutral-900 p-8 flex justify-center">
     <div class="max-w-screen-lg w-full flex flex-col gap-6">
-        <p class="font-bold text-2xl text-center">Let's Work Together</p>
+        <p class="font-bold text-2xl text-center text-black dark:text-white">Let's Work Together</p>
         <div class="flex flex-nowrap gap-5 w-full justify-center">
             <a href="/contact"
                 class="text-white text-xl font-semibold bg-[#543219] py-2 px-3"
