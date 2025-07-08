350f5719ff43f23a2b297f7f79685d566f4f35c2
Nicholas Perell <nicholasperell@gmail.com>
2025-07-08 09:40:35 -0400

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import FullBodyBg from '$lib/comps/FullBodyBg.svelte';
+</script>
+
+<FullBodyBg>
+	<div class="text-center text-lg text-black dark:text-white">
+		{page.error.message}
+	</div>
+</FullBodyBg>
