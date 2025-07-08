afd6678a8bd4bedbea9fcfd077216a39f5b44ad9
Nicholas Perell <nicholasperell@gmail.com>
2025-07-08 01:01:05 -0400

    comp for color bg to fill full page bg

---
+++ src/lib/comps/FullBodyBg.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	let { children = $bindable() } = $props();
+	let innerHeight = $state(0);
+	let pageBody: HTMLDivElement | undefined = $state();
+	let pageBodyMinHeight = $derived(innerHeight - (pageBody?.offsetTop ?? 0));
+</script>
+
+<svelte:window bind:innerHeight />
+
+<div
+	bind:this={pageBody}
+	class="flex h-full w-full shrink-0 grow flex-col bg-white dark:bg-black"
+	style:min-height={`${pageBodyMinHeight}px`}
+>
+	{@render children?.()}
+</div>
