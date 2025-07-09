4395e8c187f2f318c0fed3b8138731c7278619c0
Nicholas Perell <nicholasperell@gmail.com>
2025-07-09 17:41:54 -0400

    header shrinkage for the different screen widths

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,18 +5,18 @@
 	let slideIn = $state(false);
 </script>
 
-<div class="fixed z-10 sm:z-auto sm:relative w-full h-10 sm:h-full p-1 sm:p-4 flex flex-row flex-nowrap bg-artemis-card dark:bg-[#314F2E] items-center justify-between sm:justify-center sm:gap-12 overflow-hidden">
+<div class="fixed z-10 sm:z-auto sm:relative w-full h-10 sm:h-full p-1 sm:p-4 flex flex-row flex-nowrap bg-artemis-card dark:bg-[#314F2E] items-center justify-between sm:justify-center sm:gap-6 md:gap-12 overflow-hidden">
 	<div class="block sm:hidden invisible">
 		<img class="h-8" src="/imgs/goddess-icon.png" alt="Artemis Icon"/>
 	</div>
-	<a class="hidden sm:block text-xl font-semibold text-white align-middle" href="/work">Experience</a>
-	<a class="hidden sm:block text-xl font-semibold text-white align-middle" href="/resume">Resume</a>
-	<a class="flex flex-row flex-nowrap items-center gap-5" href="/">
-		<img class="hidden sm:block h-16" src="/imgs/goddess-icon.png" alt="Artemis Icon"/>
-		<p class="text-2xl sm:text-5xl font-bold text-white align-middle">Nicholas Perell</p>
+	<a class="hidden sm:block text-lg md:text-xl font-semibold text-white align-middle" href="/work">Experience</a>
+	<a class="hidden sm:block text-lg md:text-xl font-semibold text-white align-middle" href="/resume">Resume</a>
+	<a class="flex flex-row flex-nowrap items-center gap-1 lg:gap-5" href="/">
+		<img class="hidden h-8 md:h-12 sm:block lg:h-16" src="/imgs/goddess-icon.png" alt="Artemis Icon"/>
+		<p class="text-xl md:text-2xl lg:text-5xl font-bold text-white align-middle">Nicholas Perell</p>
 	</a>
-	<a class="hidden sm:block text-xl font-semibold text-white align-middle" href="/posts">Blog</a>
-	<a class="hidden sm:block text-xl font-semibold text-white align-middle" href="/contact">Contact</a>
+	<a class="hidden sm:block text-lg md:text-xl font-semibold text-white align-middle" href="/posts">Blog</a>
+	<a class="hidden sm:block text-lg md:text-xl font-semibold text-white align-middle" href="/contact">Contact</a>
 	<button class="sm:hidden block self-end"
 		onclick={() => slideIn = !slideIn}>
 		<img class="h-8" src="/imgs/goddess-icon.png" alt="Artemis Icon"/>
