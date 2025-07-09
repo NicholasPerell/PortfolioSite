15018e2a93ce7e534a25f9bb0a00441ff5fcefcf
Nicholas Perell <nicholasperell@gmail.com>
2025-07-09 17:24:19 -0400

    Slide in sidebar after certain size

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+
+	let slideIn = $state(false);
 </script>
 
 <div class="fixed z-10 sm:z-auto sm:relative w-full h-10 sm:h-full p-1 sm:p-4 flex flex-row flex-nowrap bg-artemis-card dark:bg-[#314F2E] items-center justify-between sm:justify-center sm:gap-12 overflow-hidden">
@@ -15,9 +17,21 @@
 	</a>
 	<a class="hidden sm:block text-xl font-semibold text-white align-middle" href="/posts">Blog</a>
 	<a class="hidden sm:block text-xl font-semibold text-white align-middle" href="/contact">Contact</a>
-	<button class="sm:hidden block self-end">
+	<button class="sm:hidden block self-end"
+		onclick={() => slideIn = !slideIn}>
 		<img class="h-8" src="/imgs/goddess-icon.png" alt="Artemis Icon"/>
 	</button>
+</div>
+
+<div 
+	class="fixed bg-artemis-card dark:bg-[#314F2E] sm:hidden top-10 z-30 p-2 min-w-[34%] w-fit h-full flex flex-col flex-nowrap transition-all duration-500"
+	class:right-0={slideIn}
+	class:-right-full={!slideIn}
+>
+	<a class="text-lg text-white" href="/work">Experience</a>
+	<a class="text-lg text-white" href="/resume">Resume</a>
+	<a class="text-lg text-white" href="/posts">Blog</a>
+	<a class="text-lg text-white" href="/contact">Contact</a>
 </div>
 
 <div class="bg-artemis-card dark:bg-[#314F2E] sm:hidden h-10"></div>
