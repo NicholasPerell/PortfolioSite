2e8a8c48ac33be02035c0edc0380be3efb129c5f
Nicholas Perell <nicholasperell@gmail.com>
2025-07-09 11:42:25 -0400

    Fix click area for experience items

--- src/lib/comps/ExperienceCard.svelte
+++ src/lib/comps/ExperienceCard.svelte
@@ -4,46 +4,45 @@
 	let { props } = $props();
 </script>
 
-<div class="group flex">
-	<div class="relative aspect-[4/3] overflow-hidden w-2/3">
-		<a href={props.uri}>
-			<img
-				class="absolute h-full w-full object-cover object-center"
-				src={props.thumbnail.src}
-				alt={props.thumbnail.alt}
-			/>
-			<img
-				class="absolute h-full w-full object-cover object-center opacity-0 duration-500 group-hover:opacity-100 group-hover:delay-500"
-				src={props.hoverPreview.src}
-				alt={props.hoverPreview.alt}
-			/>
-			
-		</a>
-	</div>
-	<div
-		class="flex w-full"
-	>
-		<div class="flex h-full w-full flex-col items-center"
-			class:py-4={!props.logline}
+<a href={props.uri}>
+	<div class="group flex">
+		<div class="relative aspect-[4/3] overflow-hidden w-2/3">
+				<img
+					class="absolute h-full w-full object-cover object-center"
+					src={props.thumbnail.src}
+					alt={props.thumbnail.alt}
+				/>
+				<img
+					class="absolute h-full w-full object-cover object-center opacity-0 duration-500 group-hover:opacity-100 group-hover:delay-500"
+					src={props.hoverPreview.src}
+					alt={props.hoverPreview.alt}
+				/>
+		</div>
+		<div
+			class="flex w-full"
 		>
-			<div class="flex flex-col w-full items-start justify-start pl-8"
-				class:pt-4={props.logline}
-				class:pb-3={props.logline}
+			<div class="flex h-full w-full flex-col items-center"
+				class:py-4={!props.logline}
 			>
-				<p class="text-3xl font-bold">{props.title}</p>
-				<p class="text-xl italic">{props.role}</p>
-				<p class="text-lg">{props.hoverText}</p>
-			</div>
-			{#if props.logline}
-				<div class="flex w-full h-full items-center justify-center gap-3 px-8">
-					<p>{props.logline}</p>
+				<div class="flex flex-col w-full items-start justify-start pl-8"
+					class:pt-4={props.logline}
+					class:pb-3={props.logline}
+				>
+					<p class="text-3xl font-bold">{props.title}</p>
+					<p class="text-xl italic">{props.role}</p>
+					<p class="text-lg">{props.hoverText}</p>
 				</div>
-			{/if}
-			<div class="flex w-full h-full items-center justify-center gap-3">
-				{#each props.tools as tool}
-					<ToolIconImage length={40} {tool} />
-				{/each}
+				{#if props.logline}
+					<div class="flex w-full h-full items-center justify-center gap-3 px-8">
+						<p>{props.logline}</p>
+					</div>
+				{/if}
+				<div class="flex w-full h-full items-center justify-center gap-3">
+					{#each props.tools as tool}
+						<ToolIconImage length={40} {tool} />
+					{/each}
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
+</a>
