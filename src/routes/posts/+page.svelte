a13e236449f70003a7460c47652e5f3c00e8ed15
Nicholas Perell <nicholasperell@gmail.com>
2025-07-10 14:14:46 -0400

    overflow-hidden for posts page

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -8,24 +8,26 @@
 </script>
 
 <FullBodyBg>
-    <div class="w-fit max-w-screen-lg flex flex-col self-center content-center">
-        <div class="flex flex-row flex-nowrap gap-2 items-center px-2 pt-4 pb-6">
-            <div class="h-0.5 w-7 bg-black dark:bg-white"></div>
-            <p class="font-serif font-bold text-3xl text-black dark:text-white">Posts</p>
-            <div class="h-0.5 w-7 bg-black dark:bg-white"></div>
-        </div>
-        <div class="px-2 flex flex-col sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pb-2">
-            {#each posts as post}
-                <BlogPostCard 
-                    slug={post.slug} 
-                    title={post.title}
-                    blurb={post.blurb}
-                    written={post.written}
-                    thumbnail={post.thumbnail}
-                    tags={post.tags ?? []}
-                    tools={post.tools ?? []}
-                />
-            {/each}
+    <div class="h-full w-full max-w-[100vw] overflow-hidden">
+        <div class="w-fit max-w-screen-lg flex flex-col self-center content-center">
+            <div class="flex flex-row flex-nowrap gap-2 items-center px-2 pt-4 pb-6">
+                <div class="h-0.5 w-7 bg-black dark:bg-white"></div>
+                <p class="font-serif font-bold text-3xl text-black dark:text-white">Posts</p>
+                <div class="h-0.5 w-7 bg-black dark:bg-white"></div>
+            </div>
+            <div class="px-2 flex flex-col sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pb-2">
+                {#each posts as post}
+                    <BlogPostCard 
+                        slug={post.slug} 
+                        title={post.title}
+                        blurb={post.blurb}
+                        written={post.written}
+                        thumbnail={post.thumbnail}
+                        tags={post.tags ?? []}
+                        tools={post.tools ?? []}
+                    />
+                {/each}
+            </div>
         </div>
     </div>
 </FullBodyBg>
