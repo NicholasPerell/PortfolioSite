c1d7647964d2c4f4d9f95291448c8b70b1dce6c3
Nicholas Perell <nicholasperell@gmail.com>
2025-07-09 23:54:51 -0400

    scroll through for posts page

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -8,14 +8,14 @@
 </script>
 
 <FullBodyBg>
-    <div class="flex flex-row flex-nowrap gap-2 items-center">
+    <div class="flex flex-row flex-nowrap gap-2 items-center px-2 pt-4 pb-6">
         <div class="h-0.5 w-7 bg-black dark:bg-white"></div>
         <p class="font-serif font-bold text-3xl text-black dark:text-white">Posts</p>
         <div class="h-0.5 w-7 bg-black dark:bg-white"></div>
     </div>
-    <div class="w-full flex flex-row flex-wrap gap-6">
+    <div class="w-full px-2 flex flex-row flex-wrap gap-6 pb-2">
         {#each posts as post}
-            <div class="w-1/5">
+            <div class="sm:w-1/3 md:w-1/4 lg:w-1/5">
                 <BlogPostCard 
                     slug={post.slug} 
                     title={post.title}
